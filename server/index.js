8bb16099e75309441ae4cfed6e712c8c0ab28789
sapienc <55036936+sapienc@users.noreply.github.com>
2021-01-31 16:51:17 +0300

    Build experiments
    Tried to build and run the app on local machine

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser'); // Для парсинга body в объекты JS у запросов для фреймворка Express
 const path = require('path');
 
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 /* ----------------------------------- */
+
+const PORT = process.env.PORT || 4000;
+const IS_DEV = process.env.NODE_ENV !== 'production';
 
 /* ========== [НАСТРОЙКИ ПРИЛОЖЕНИЯ] ========== */
 const app = express();
@@ -11,12 +16,6 @@ const app = express();
 app.use(require('cors')()); // для возможности обрабатывать запросы с других доменов (допустим клиент на другом домене)
 app.use(bodyParser.urlencoded({ extended: true })); // защищаем URL от некоторых символов
 app.use(bodyParser.json()); // чтобы превращать пришедшие с responce'a json объекты в объекты javascript
-
-const PORT = process.env.PORT || 4000;
-const IS_DEV = process.env.NODE_ENV !== 'production';
-
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 
 /* ========== [Для использования всех ядер процессора при запуске команд Node.JS, если это возможно] ========== */
 if (!IS_DEV && cluster.isMaster) {
@@ -31,8 +30,25 @@ if (!IS_DEV && cluster.isMaster) {
         console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
     });
 }
-else {
-    app.use(express.static('../client/build/')); // используем наше построенное приложение как статику
+else { 
+
+    // console.log(__dirname);
+    // console.log(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    // console.log(path.join(__dirname, '..', 'client', 'build', 'static', 'media'));
+
+    // используем наше построенное приложение как статику
+    app.use(express.static( path.resolve(__dirname, '../client', 'build') ));
+
+    // app.use('/mediafiles', express.static(path.join(__dirname, '..', 'client', 'build', 'static', 'media')));
+    // app.use('/', (req, res, next) => {
+    //     express.static(path.resolve('/static'))(req, res, next);
+    // });
+
+    // app.use('/', (req, res, next) => {
+    //     express.static(
+    //         path.resolve(__dirname, '..', 'client', 'build', 'static')(req, res, next)
+    //     );
+    // });
 
     // Отлавливаем и обрабатываем все-возможные маршруты нашего забилденного приложения (таким образом маршруты приложения с клиента также будут работать)
     app.get('*', (request, response) => {
